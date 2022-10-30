6cb616da015ec86950d2118f09a0951a8839ca7c
Avery Blanchard <agb2178@columbia.edu>
2022-10-29 22:11:41 -0400

    Outline container ima init process
    Signed-off-by: Avery Blanchard <agb2178@columbia.edu>

--- container_ima.c
+++ container_ima.c
@@ -11,9 +11,14 @@
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <linux/printk.h>
+#include <linux/ima.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <keys/system_keyring.h>
 
 #define MODULE_NAME "ContainerIMA"
 
+struct tpm_chip *container_ima_tpm;
 struct mmap_args_t {
 	void *addr;
 	size_t length;
@@ -22,6 +27,67 @@ struct mmap_args_t {
 	int fd;
 	off_t offset
 };
+/*
+ * ima_hash_setup
+ *
+ * Set up container IMA hashing/crypto algorithm 
+ * use the alorithm that the kernel IMA defaulted 
+ * to
+ */
+void ima_hash_setup() 
+{
+	
+	
+}
+/*
+ * container_ima_setup
+ *
+ * Set up environment to initalize container IMA
+ */
+void container_ima_setup()
+{
+	ima_hash_setup();
+
+}
+/*
+ * container_ima_init
+ *
+ * Initalize container IMA
+ */
+int container_ima_init() 
+{
+	int ret;
+
+	container_ima_tpm = tpm_default_chip();
+	if (!container_ima_chip)
+		pr_info("No TPM chip detected, bypassing\n");
+	ret = integrity_init_keyring(INTEGRITY_KEYRING_IMA); // decide whether to use normal IMA keyring or not. Think about how this will affect normal IMA
+
+	if (ret)
+		return ret;
+	ret = container_ima_crypto_init(); // iterate over PCR banks and init the algorithms per bank  
+
+	if (ret)
+		return ret;
+
+	ret = container_ima_fs_init(); // set up directory for per container Measurment Log
+
+	if (ret) 
+		return ret;
+
+	container_ima_policy_init();
+
+	return ret;
+}
+/* check_container_map 
+ * 
+ * Determine whether mmap call was from a container or host process  
+ */
+int check_container_map() 
+{
+
+
+}
 
 /* mapping of id to system call arguments */
 BPF_HASH(active_mmap_args_map, uint64, struct mmap_args_t);
@@ -68,19 +134,27 @@ int syscall__probe_ret_mmap(struct pt_regs *ctx)
 	 * then 
 	 * 	1. access argument cache
 	 * 	2. call functions to create hash digest, extend,
-	 * 		and send to TPM for IMA.
+	 * 		and send to TPM for IMA per container.
 	 */
+	
+
 
 }
 static int container_ima_init(void)
 {
-	/* Initialize system call probe */
+	/* Start container IMA */
+	int ret;
+	container_ima_setup();
+	ret = container_ima_init();
+
+	return ret;
 }
 
 static void container_ima_init(void)
 {
-	/* End system call probe & clean up */
+	return NULL;
 }
+
 
 module_init(container_ima_init);
 module_exit(container_ima_exit);
