caf7a59c55dc1576a8f6719337095b2020a757fe
Avery Blanchard <agb2178@columbia.edu>
2023-05-03 15:27:28 -0400

    Fix probe formatting
    Signed-off-by: Avery Blanchard <agb2178@columbia.edu>

--- probe.bpf.c
+++ probe.bpf.c
@@ -67,69 +67,71 @@ int BPF_KPROBE_SYSCALL(kprobe___sys_mmap, void *addr, unsigned long length, unsi
     
     if (prot & 0x04) {
     
-	    if (flags & MAP_ANONYMOUS)
-		    return 0;
-    task = (void *) bpf_get_current_task();
+        if (flags & MAP_ANONYMOUS)
+		return 0;
 
-    bpf_printk("Integrity measurement for fd %d\n", fd);
+        task = (void *) bpf_get_current_task();
 
-    key = 0; 
-    data = (struct ima_data *) bpf_map_lookup_elem(&map, &key);
-    if (!data) {
-	    bpf_printk("Map element lookup failed\n");
-	    return 0;
-    }
+        bpf_printk("Integrity measurement for fd %d\n", fd);
 
-    data->inum = BPF_CORE_READ(task, nsproxy, cgroup_ns, ns.inum);
-    bpf_printk("BPF INUM %d\n", data->inum);
-    data->host = bpfmeasurement(data->inum);
+        key = 0; 
+        data = (struct ima_data *) bpf_map_lookup_elem(&map, &key);
+        if (!data) {
+                bpf_printk("Map element lookup failed\n");
+                return 0;
+        }
 
-    bpf_printk("INUM comparision returns %d\n", data->host);
-    if (data->host == data->inum) {
+        data->inum = BPF_CORE_READ(task, nsproxy, cgroup_ns, ns.inum);
+        bpf_printk("BPF INUM %d\n", data->inum);
+        data->host = bpfmeasurement(data->inum);
 
-	    bpf_printk("PRE RETRIEVE FILE\n");
-	    file = container_ima_retrieve_file(fd);
-	   bpf_printk("POST CHECK\n"); 	    
-	    if (file) {
+        bpf_printk("INUM comparision returns %d\n", data->host);
+        if (data->host == data->inum) {
 
-                        bpf_printk("FILE RETRIEVED\n");
-		        data->inode = BPF_CORE_READ(file, f_inode);
-                        data->f_name = BPF_CORE_READ(file, f_path.dentry, d_name.name);
+                bpf_printk("PRE RETRIEVE FILE\n");
+                file = container_ima_retrieve_file(fd);
+                bpf_printk("POST CHECK\n"); 	    
+                if (file) {
 
-			bpf_printk("FILE INODE AND DENTRY NAME\n");
-			//data->hash = ima_hash_setup();
+                                bpf_printk("FILE RETRIEVED\n");
+                                data->inode = BPF_CORE_READ(file, f_inode);
+                                data->f_name = BPF_CORE_READ(file, f_path.dentry, d_name.name);
 
-			bpf_printk("HASH SET UP\n");
-                        data->f_flags = BPF_CORE_READ(file, f_flags); 
-			if (data->f_flags & O_DIRECT) {
+                                bpf_printk("FILE INODE AND DENTRY NAME\n");
+                                //data->hash = ima_hash_setup();
+
+                                bpf_printk("HASH SET UP\n");
+                                data->f_flags = BPF_CORE_READ(file, f_flags); 
+                                if (data->f_flags & O_DIRECT) {
+                                        return 0;
+                                }
+                                bpf_printk("FLAGS \n");
+                                data->f_mode = BPF_CORE_READ(file, f_mode);
+                                if (!(data->f_mode & FMODE_READ)) {
+                                        return 0;
+                                }
+                                bpf_printk("MODE\n");
+                                
+                                shash = ima_shash_init();
+                                bpf_printk("SHASH INIT\n");
+                                
+                                data->cra_init = BPF_CORE_READ(shash,base.__crt_alg, cra_init);
+                                data->base = BPF_CORE_READ(shash, base);
+                                data->digest = ima_crypto(file, &data->base, data->cra_init);
+
+                                strncpy(&data->tpm_digest->digest[0], &data->hash.hdr.digest[0], sizeof(data->hash.hdr.digest));
+                                
+                                data->tpm_digest->alg_id = TPM_ALG_SHA256;
+                                
+                                ret = ima_pcr_extend(data->tpm_digest, 10);
+                                
                                 return 0;
+
                         }
-                        bpf_printk("FLAGS \n");
-			data->f_mode = BPF_CORE_READ(file, f_mode);
-                        if (!(data->f_mode & FMODE_READ)) {
-                                return 0;
-                        }
-			bpf_printk("MODE\n");
-			
-			shash = ima_shash_init();
-			bpf_printk("SHASH INIT\n");
-			
-			data->cra_init = BPF_CORE_READ(shash,base.__crt_alg, cra_init);
-			data->base = BPF_CORE_READ(shash, base);
-			data->digest = ima_crypto(file, &data->base, data->cra_init);
 
-			strncpy(&data->tpm_digest->digest[0], &data->hash.hdr.digest[0], sizeof(data->hash.hdr.digest));
-			
-			data->tpm_digest->alg_id = TPM_ALG_SHA256;
-			
-			ret = ima_pcr_extend(data->tpm_digest, 10);
-			
-			return 0;
+                 }
 
-	    }
-
-    }
-    }
+        }
     return 0;
 
 }
