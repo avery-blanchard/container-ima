1b4c16a8d0ca36440591695ab708a078746a7227
Avery Blanchard <avery.blanchard@ibm.com>
2023-07-26 18:51:45 +0000

    Update namespace
    Signed-off-by: Avery Blanchard <avery.blanchard@ibm.com>

--- probe.bpf.c
+++ probe.bpf.c
@@ -30,10 +30,10 @@ int BPF_PROG(mmap_hook, struct file *file, unsigned int reqprot,
     if (!file) 
 	return 0;
     
-    if (prot & PROT_EXEC) {
+    if (prot & PROT_EXEC || reqprot & PROT_EXEC) {
 	
 	task = (void *) bpf_get_current_task();
-        ns = BPF_CORE_READ(task, nsproxy, cgroup_ns, ns.inum);
+        ns = BPF_CORE_READ(task, nsproxy, uts_ns, ns.inum);
 	
 	struct ebpf_data data = { .file = file, .ns = ns };
 	
